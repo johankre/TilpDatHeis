4d09b04cef163ed5e5927d22c63d57fa53a5a31a
Terralink2408 <leopal24@gmail.com>
2023-03-09 11:38:55 +0100

    ny sleep

--- skeleton_project/source/driver/elevDoor.c
+++ skeleton_project/source/driver/elevDoor.c
@@ -4,11 +4,15 @@
 
 
 #include "elevio.h"
+#include "driver/elevio.h"
 
 void openDoor() {
     elevio_doorOpenLamp(1);
     nanosleep(&(struct timespec){0, 3*1000*1000*1000}, NULL);
-    elevio_doorOpenLamp(0);
+    while elevio_obstruction() == 1 { //ser etter obstruksjon
+        nanosleep(&(struct timespec){0, 3*1000*1000*1000}, NULL);
+    }
+    elevio_doorOpenLamp(0); //holder døren åpen i 3sek og lukker etterpå
 }
 
 //legg til openDoor i relevante filer
