5b1f22f5f83dbcafa588a3f89600832a4304e406
Terralink2408 <leopal24@gmail.com>
2023-03-10 13:38:13 +0100

    opendoor-funksjon endret

--- skeleton_project/source/driver/elevDoor.c
+++ skeleton_project/source/driver/elevDoor.c
@@ -11,9 +11,9 @@
 
 void openDoor() {
     elevio_doorOpenLamp(1);
-    nanosleep(&(struct timespec){0, 3*1000*1000*1000}, NULL);
+    nanosleep(&(struct timespec){3, 0}, NULL);
     while elevio_obstruction() == 1 { //ser etter obstruksjon
-        nanosleep(&(struct timespec){0, 3*1000*1000*1000}, NULL);
+        
     }
     elevio_doorOpenLamp(0); //holder døren åpen i 3sek og lukker etterpå
 }
