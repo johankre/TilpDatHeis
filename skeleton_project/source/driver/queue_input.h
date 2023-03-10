0aa61c021f7365b573a459c7ff85964763f2e8db
johankre <johannes.k.reitan@gmail.com>
2023-03-10 14:17:41 +0100

    header struct

--- skeleton_project/source/driver/queue_input.h
+++ skeleton_project/source/driver/queue_input.h
@@ -1,17 +1,3 @@
 #pragma once
 
-typedef enum { 
-    QUEUE_DOWN   = 0,
-    QUEUE_UP     = 1
-} QueueDirection;
-
-
-typedef struct{
-    int *p_queue;
-    int next;
-    int last;
-    QueueDirection direction;
-}Queue; 
-
-
 void input_floor(Queue*);
