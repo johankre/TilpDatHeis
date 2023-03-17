7839a274c82b6012dc36a80e232c39d68e0f03ff
Terralink2408 <leopal24@gmail.com>
2023-03-17 12:33:53 +0100

    oppdatert Main i forhold til JOE

--- skeleton_project/source/main.c
+++ skeleton_project/source/main.c
@@ -21,19 +21,36 @@ int main(){
     dimAll();
 
     while(1){
-        int floor = elevio_floorSensor();
+       int floor = elevio_floorSensor();
         printf("floor: %d \n",floor);
 
-        
 
+        // Oppdaterer queue last og queue next
+        update_queue_internal(p_queue);
+        printf("a");
+
+        for(int f = 0; f < N_FLOORS; f++){
+            for(int b = 0; b < N_BUTTONS; b++){
+                int btnPressed = elevio_callButton(f, b);
+                elevio_buttonLamp(f, b, btnPressed);
+            }
+        }
+        printf("b");
+
+        if(elevio_obstruction()){
+            elevio_stopLamp(1);
+        } else {
+            elevio_stopLamp(0);
+        }
+        
+        printf("c");
+        // Utfører nødstopp
         if(elevio_stopButton()){
             stopButtonCaled();
             clear_queue(p_queue);
         }
-
         
-        
-
+        printf("d");
         nanosleep(&(struct timespec){0, 20*1000*1000}, NULL);
     }
 
